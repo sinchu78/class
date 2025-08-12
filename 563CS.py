908067ee7add71d898cf0a40f02de143f4c44cb6
sinchu78 <sinchupappa78@gmail.com>
2025-08-11 18:09:33 -1200

    Add files via upload

---
+++ 563CS.py
@@ -0,0 +1,8 @@
+a=input("enter value for a")
+b=input("enter value for b")
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a and b aer equal")
+else:
+    print("a is less than b")
